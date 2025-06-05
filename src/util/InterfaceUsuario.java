70c03b59e12dbe32f073ee9c8aae69ef85cd5497
diogoMprado <dmessiasdoprado@gmail.com>
2025-06-05 17:59:56 -0300

    Update InterfaceUsuario.java

--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -8,42 +8,42 @@ public class InterfaceUsuario {
 
     // Recebe o valor do imóvel, com o teste "SE for maior que zero"
     public Double valorImovel(){
-        while(true){
+        double valor;
+        do {
             System.out.print("Informe o valor do Imóvel: ");
-            double valor = sc.nextDouble();
-            if (valor > 0){
-                return valor;
-            }
-            else{
+            valor = sc.nextDouble();
+            if (valor <= 0) {
                 System.out.println("Informe um valor válido!");
             }
-        }
+        } while (valor <= 0);
+        return valor;
     }
 
     // Recebe a quantidade de meses do financiamento, com o teste "SE for maior que zero"
     public int prazoFinanciamento(){
-        while(true){
+        int prazo;
+        do {
             System.out.print("Informe o Prazo de Financiamento: ");
-            int prazo = sc.nextInt();
-            if (prazo > 0){
-                return prazo;
-            }
-            else{
+            prazo = sc.nextInt();
+            if (prazo <= 0){
                 System.out.println("Informe um Prazo de Financiamento maior que zero!");
             }
         }
+        while (prazo <= 0);
+        return prazo;
     }
+
     // Recebe o valor da taxa de juros anual, com o teste "SE for maior que zero"
     public Double taxaJurosAnual(){
-        while(true) {
+        double juros;
+        do {
             System.out.print("Informe o taxa juros: ");
-            double juros = sc.nextDouble();
-            if (juros > 0 && juros <= 1000) {
-                return juros;
-            }
-            else {
+            juros = sc.nextDouble();
+            if (juros <= 0 || juros > 1000) {
                 System.out.println("Informe um valor válido!");
             }
         }
+        while(juros <= 0 || juros > 1000);
+        return juros;
     }
 }
